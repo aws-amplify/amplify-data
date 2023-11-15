b35a8ff5f2d25011a69637fade94b1707141330c
Aaron S <stocaaro@stocad.com>
2023-11-15 08:34:05 -0600

    fix: Remove an experiment introduced by prior change

--- packages/data-schema/src/Brand.ts
+++ packages/data-schema/src/Brand.ts
@@ -11,8 +11,3 @@ export function brandTarget<T extends object, TBrand extends string>(
 ): Brand<T, TBrand> {
   return { ...target, [brand]: brandName };
 }
-
-export type Unbrand<T extends object, B extends Brand<T, string>> = Omit<
-  B,
-  brand
->;
