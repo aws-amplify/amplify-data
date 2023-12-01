3a4bae7d6f5bc3592874c2eda450e988acae9d6a
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2023-12-01 18:18:10 -0500

    add benches

---
+++ packages/benches/schema.bench.ts
@@ -0,0 +1,22 @@
+import { bench } from '@arktype/attest';
+import { a, ClientSchema } from '@aws-amplify/data-schema';
+
+bench('baseline', () => {}).types([0, 'instantiations']);
+
+bench('basic schema', () => {
+  a.schema({
+    Post: a.model({
+      title: a.string().required(),
+    }),
+  });
+}).types([3045, 'instantiations']);
+
+bench('basic schema w client types', () => {
+  const s = a.schema({
+    Post: a.model({
+      title: a.string().required(),
+    }),
+  });
+
+  type _ = ClientSchema<typeof s>;
+}).types([86925, 'instantiations']);
