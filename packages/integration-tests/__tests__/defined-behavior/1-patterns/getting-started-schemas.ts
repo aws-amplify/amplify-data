8b03f3de8428c56ca1a8fdb15eff2f6008cf9369
Jon Wire <iambipedal@gmail.com>
2024-03-01 14:42:35 -0600

    checkpoint

--- packages/integration-tests/__tests__/defined-behavior/1-patterns/getting-started-schemas.ts
+++ packages/integration-tests/__tests__/defined-behavior/1-patterns/getting-started-schemas.ts
@@ -1,4 +1,8 @@
 import { a, ClientSchema } from '@aws-amplify/data-schema';
+import { Amplify } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/api';
+import { buildConfig } from '../../utils/build-amplify-config';
+
 // import {
 //   type Expect,
 //   type Equal,
@@ -9,19 +13,65 @@ import { a, ClientSchema } from '@aws-amplify/data-schema';
 //   __modelMeta__,
 // } from '@aws-amplify/data-schema-types';
 
+/**
+ * @param value Value to be returned. Will be `awaited`, and can
+ * therefore be a simple JSON value or a `Promise`.
+ * @returns
+ */
+function mockApiResponse(client: any, value: any) {
+  return jest.spyOn(client, 'graphql').mockImplementation(async () => {
+    console.log('mocked impl');
+    const result = await value;
+    return {
+      body: {
+        json: () => result,
+      },
+    };
+  });
+}
+
 describe('getting started guides', () => {
-  describe('nextjs app router server components', () => {
-    test('add data Todo schema', async () => {
-      const schema = a.schema({
-        Todo: a
-          .model({
-            content: a.string(),
-            done: a.boolean(),
-            priority: a.enum(['low', 'medium', 'high']),
-          })
-          .authorization([a.allow.owner(), a.allow.public().to(['read'])]),
+  describe('Todo schema', () => {
+    const schema = a.schema({
+      Todo: a
+        .model({
+          content: a.string(),
+          done: a.boolean(),
+          priority: a.enum(['low', 'medium', 'high']),
+        })
+        .authorization([a.allow.owner(), a.allow.public().to(['read'])]),
+    });
+    type Schema = ClientSchema<typeof schema>;
+
+    test('produces correct graphql', async () => {
+      const transformed = schema.transform();
+      expect(transformed.schema).toMatchSnapshot();
+    });
+
+    test('produces correct model intro schema', async () => {
+      const config = await buildConfig(schema);
+      expect(config).toMatchSnapshot();
+    });
+
+    test('can create', async () => {
+      const config = await buildConfig(schema);
+      Amplify.configure(config);
+
+      const client = await generateClient<Schema>();
+      const spy = mockApiResponse(client, {
+        data: {
+          createTodo: {
+            something: 'else',
+          },
+        },
       });
-      expect(schema.transform()).toMatchSnapshot();
+
+      const { data } = await client.models.Todo.create({
+        content: 'basic content',
+      });
+
+      expect(spy.mock.calls).toMatchSnapshot();
+      expect(data).toMatchSnapshot();
     });
   });
 });
--- packages/integration-tests/jest.config.js
+++ packages/integration-tests/jest.config.js
@@ -2,5 +2,9 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["test-d.ts"], // don't directly execute these files. They'll be tested via their `test.ts` counterparts
+  testPathIgnorePatterns: [
+    "test-d.ts", // don't directly execute these files. They'll be tested via their `test.ts` counterparts
+    "fixtures",
+    "utils"
+  ], 
 };
