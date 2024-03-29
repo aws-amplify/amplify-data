41b9f557c184106726ba80fcd3508a1f4cdf2ce9
Hui Zhao <zhohz@amazon.com>
2024-03-29 10:30:00 -0700

    fix: remove .returns() modifier from custom subscription

--- packages/integration-tests/__tests__/pseudo-client.test-d.ts
+++ packages/integration-tests/__tests__/pseudo-client.test-d.ts
@@ -11,6 +11,7 @@ import {
   type GraphQLFormattedError,
   __modelMeta__,
   Prettify,
+  ExtractModelMeta,
 } from '@aws-amplify/data-schema-types';
 
 type FilteredKeys<T> = {
@@ -265,73 +266,26 @@ describe('client', () => {
     type test2 = Expect<Equal<Response2Type, Expected2>>;
   });
 
-  test('subscription - no args', async () => {
-    const schema = a
-      .schema({
-        Post: a.model({
-          title: a.string(),
-          liked: a.boolean(),
-        }),
+  describe('Subscription', () => {
+    test('without .arguments() modifier', async () => {
+      const schema = a
+        .schema({
+          Post: a.model({
+            title: a.string(),
+            liked: a.boolean(),
+          }),
 
-        onCreateOrUpdatePost: a
-          .subscription()
-          .for(a.ref('Post').mutations(['create', 'update']))
-          .returns(a.ref('Post'))
-          .handler(a.handler.function('onCreateOrUpdatePost')),
-      })
-      .authorization([a.allow.public()]);
+          onCreateOrUpdatePost: a
+            .subscription()
+            .for(a.ref('Post').mutations(['create', 'update']))
+            .handler(a.handler.function('onCreateOrUpdatePost')),
+        })
+        .authorization([a.allow.public()]);
 
-    type Schema = ClientSchema<typeof schema>;
-    const client = generateClient<Schema>();
+      type Schema = ClientSchema<typeof schema>;
+      const client = generateClient<Schema>();
 
-    client.subscriptions.onCreateOrUpdatePost().subscribe({
-      next: (data) => {
-        type ResponseType = Prettify<typeof data>;
-        type ExpectedType = {
-          readonly id: string;
-          readonly createdAt: string;
-          readonly updatedAt: string;
-          title?: string | null;
-          liked?: boolean | null;
-        } | null;
-        type test = Expect<Equal<ResponseType, ExpectedType>>;
-      },
-      error: (error) => {},
-    });
-
-    // Accepts options
-    client.subscriptions.onCreateOrUpdatePost({
-      authMode: 'apiKey',
-      authToken: 'abc123',
-      headers: { someHeader: 'someValue' },
-    });
-  });
-
-  test('subscription - with args', async () => {
-    const schema = a
-      .schema({
-        Post: a.model({
-          title: a.string(),
-          liked: a.boolean(),
-        }),
-
-        onCreateOrUpdatePost: a
-          .subscription()
-          .for(a.ref('Post').mutations(['create', 'update']))
-          .arguments({
-            postId: a.string().required(),
-          })
-          .returns(a.ref('Post'))
-          .handler(a.handler.function('onCreateOrUpdatePost')),
-      })
-      .authorization([a.allow.public()]);
-
-    type Schema = ClientSchema<typeof schema>;
-    const client = generateClient<Schema>();
-
-    const sub = client.subscriptions
-      .onCreateOrUpdatePost({ postId: 'abc' })
-      .subscribe({
+      client.subscriptions.onCreateOrUpdatePost().subscribe({
         next: (data) => {
           type ResponseType = Prettify<typeof data>;
           type ExpectedType = {
@@ -346,15 +300,294 @@ describe('client', () => {
         error: (error) => {},
       });
 
-    // Accepts options
-    client.subscriptions.onCreateOrUpdatePost(
-      { postId: 'abc' },
-      {
+      // Accepts options
+      client.subscriptions.onCreateOrUpdatePost({
         authMode: 'apiKey',
         authToken: 'abc123',
         headers: { someHeader: 'someValue' },
-      },
-    );
+      });
+    });
+
+    test('with .arguments() modifier', async () => {
+      const schema = a
+        .schema({
+          Post: a.model({
+            title: a.string(),
+            liked: a.boolean(),
+          }),
+
+          onCreateOrUpdatePost: a
+            .subscription()
+            .for(a.ref('Post').mutations(['create', 'update']))
+            .arguments({
+              postId: a.string().required(),
+            })
+            .handler(a.handler.function('onCreateOrUpdatePost')),
+        })
+        .authorization([a.allow.public()]);
+
+      type Schema = ClientSchema<typeof schema>;
+      const client = generateClient<Schema>();
+
+      type ModelMeta =
+        ExtractModelMeta<Schema>['customOperations']['onCreateOrUpdatePost'];
+
+      const sub = client.subscriptions
+        .onCreateOrUpdatePost({ postId: 'abc' })
+        .subscribe({
+          next: (data) => {
+            type ResponseType = Prettify<typeof data>;
+            type ExpectedType = {
+              readonly id: string;
+              readonly createdAt: string;
+              readonly updatedAt: string;
+              title?: string | null;
+              liked?: boolean | null;
+            } | null;
+            type test = Expect<Equal<ResponseType, ExpectedType>>;
+          },
+          error: (error) => {},
+        });
+
+      // Accepts options
+      client.subscriptions.onCreateOrUpdatePost(
+        { postId: 'abc' },
+        {
+          authMode: 'apiKey',
+          authToken: 'abc123',
+          headers: { someHeader: 'someValue' },
+        },
+      );
+    });
+
+    test(".for() resource's return type is a model field", async () => {
+      const schema = a
+        .schema({
+          createString: a
+            .mutation()
+            .arguments({
+              input: a.string().required(),
+            })
+            .returns(a.string().required())
+            .handler(a.handler.function('createStringAndReturn')),
+          onCreateString: a
+            .subscription()
+            .for(a.ref('createString'))
+            .handler(a.handler.function('onCreateString')),
+        })
+        .authorization([a.allow.public()]);
+
+      type Schema = ClientSchema<typeof schema>;
+      const client = generateClient<Schema>();
+
+      client.subscriptions.onCreateString().subscribe({
+        next: (data) => {
+          type ResponseType = typeof data;
+          type ExpectedType = string;
+          type _ = Expect<Equal<ResponseType, ExpectedType>>;
+        },
+      });
+    });
+
+    test(".for() resources' return types are a model field", async () => {
+      const schema = a
+        .schema({
+          createString: a
+            .mutation()
+            .arguments({
+              input: a.string().required(),
+            })
+            .returns(a.string().required())
+            .handler(a.handler.function('createStringAndReturn')),
+          updateString: a
+            .mutation()
+            .arguments({
+              input: a.string().required(),
+            })
+            .returns(a.string().required())
+            .handler(a.handler.function('updateStringAndReturn')),
+          onCreateString: a
+            .subscription()
+            .for([a.ref('createString'), a.ref('updateString')])
+            .handler(a.handler.function('onCreateString')),
+        })
+        .authorization([a.allow.public()]);
+
+      type Schema = ClientSchema<typeof schema>;
+      const client = generateClient<Schema>();
+
+      client.subscriptions.onCreateString().subscribe({
+        next: (data) => {
+          type ResponseType = typeof data;
+          type ExpectedType = string;
+          type _ = Expect<Equal<ResponseType, ExpectedType>>;
+        },
+      });
+    });
+
+    test(".for() resource's return type is a custom type", async () => {
+      const schema = a
+        .schema({
+          createProduct: a
+            .mutation()
+            .arguments({
+              title: a.string().required(),
+              description: a.string(),
+            })
+            .returns(
+              a.customType({
+                title: a.string().required(),
+                description: a.string(),
+              }),
+            )
+            .handler(a.handler.function('createProduct')),
+          onCreateProduct: a
+            .subscription()
+            .for(a.ref('createProduct'))
+            .handler(a.handler.function('onCreateProduct')),
+        })
+        .authorization([a.allow.public()]);
+
+      type Schema = ClientSchema<typeof schema>;
+      const client = generateClient<Schema>();
+
+      client.subscriptions.onCreateProduct().subscribe({
+        next: (data) => {
+          type ResponseType = Prettify<typeof data>;
+          type ExpectedType = {
+            description?: string | null | undefined;
+            title: string;
+          } | null;
+          type _ = Expect<Equal<ResponseType, ExpectedType>>;
+        },
+      });
+    });
+
+    test(".for() resources' return types are a custom type", async () => {
+      const schema = a
+        .schema({
+          Product: a.customType({
+            title: a.string().required(),
+            description: a.string(),
+          }),
+          createProduct: a
+            .mutation()
+            .arguments({
+              title: a.string().required(),
+              description: a.string(),
+            })
+            .returns(a.ref('Product'))
+            .handler(a.handler.function('createProduct')),
+          updateProductDescription: a
+            .mutation()
+            .arguments({
+              description: a.string(),
+            })
+            .returns(a.ref('Product'))
+            .handler(a.handler.function('updateProductDescription')),
+          onProductChanges: a
+            .subscription()
+            .for([a.ref('createProduct'), a.ref('updateProductDescription')])
+            .handler(a.handler.function('onProductChanges')),
+        })
+        .authorization([a.allow.public()]);
+
+      type Schema = ClientSchema<typeof schema>;
+      const client = generateClient<Schema>();
+
+      client.subscriptions.onProductChanges().subscribe({
+        next: (data) => {
+          type ResponseType = Prettify<typeof data>;
+          type ExpectedType = {
+            description?: string | null | undefined;
+            title: string;
+          } | null;
+          type _ = Expect<Equal<ResponseType, ExpectedType>>;
+        },
+      });
+    });
+
+    test('.for() resources are a mix of CustomOperation and Mutation that return the same model type', async () => {
+      const schema = a
+        .schema({
+          Post: a.model({
+            title: a.string().required(),
+            content: a.string(),
+          }),
+          createPostWithTitlePrefix: a
+            .mutation()
+            .arguments({
+              prefix: a.string().required(),
+            })
+            .returns(a.ref('Post')),
+          onCreatePost: a
+            .subscription()
+            .for([
+              a.ref('Post').mutations(['create']),
+              a.ref('createPostWithTitlePrefix'),
+            ])
+            .handler(a.handler.function('onCreateString')),
+        })
+        .authorization([a.allow.public()]);
+
+      type Schema = ClientSchema<typeof schema>;
+      const client = generateClient<Schema>();
+
+      client.subscriptions.onCreatePost().subscribe({
+        next: (data) => {
+          type ResponseType = Prettify<typeof data>;
+          type ExpectedType = {
+            title: string;
+            readonly id: string;
+            readonly createdAt: string;
+            readonly updatedAt: string;
+            content?: string | null | undefined;
+          } | null;
+          type _ = Expect<Equal<ResponseType, ExpectedType>>;
+        },
+      });
+    });
+
+    test('.for() resources are a mix of Mutation and CustomOperation that return the same model type', async () => {
+      const schema = a
+        .schema({
+          Post: a.model({
+            title: a.string().required(),
+            content: a.string(),
+          }),
+          createPostWithTitlePrefix: a
+            .mutation()
+            .arguments({
+              prefix: a.string().required(),
+            })
+            .returns(a.ref('Post')),
+          onCreatePost: a
+            .subscription()
+            .for([
+              a.ref('createPostWithTitlePrefix'),
+              a.ref('Post').mutations(['create']),
+            ])
+            .handler(a.handler.function('onCreateString')),
+        })
+        .authorization([a.allow.public()]);
+
+      type Schema = ClientSchema<typeof schema>;
+      const client = generateClient<Schema>();
+
+      client.subscriptions.onCreatePost().subscribe({
+        next: (data) => {
+          type ResponseType = Prettify<typeof data>;
+          type ExpectedType = {
+            title: string;
+            readonly id: string;
+            readonly createdAt: string;
+            readonly updatedAt: string;
+            content?: string | null | undefined;
+          } | null;
+          type _ = Expect<Equal<ResponseType, ExpectedType>>;
+        },
+      });
+    });
   });
 
   test('custom ops are absent from models property', async () => {
@@ -368,10 +601,7 @@ describe('client', () => {
           .mutation()
           .arguments({ input: a.string() })
           .returns(a.string()),
-        mySubscription: a
-          .subscription()
-          .arguments({ input: a.string() })
-          .returns(a.string()),
+        mySubscription: a.subscription().arguments({ input: a.string() }),
       })
       .authorization([a.allow.public()]);
 
