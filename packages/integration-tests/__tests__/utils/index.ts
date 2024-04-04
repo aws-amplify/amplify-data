63354974719cfb73c262e32d901ea6b2d4403903
David McAfee <mcafd@amazon.com>
2024-04-04 12:05:05 -0700

    import response type from JS library

--- packages/integration-tests/__tests__/utils/index.ts
+++ packages/integration-tests/__tests__/utils/index.ts
@@ -1,7 +1,7 @@
 import { GraphQLError, print, parse, DocumentNode, TypeNode } from 'graphql';
 import { generateModels } from '@aws-amplify/graphql-generator';
 import { generateClient as actualGenerateClient } from 'aws-amplify/api';
-import { GraphQLAPI } from '@aws-amplify/api-graphql'; // eslint-disable-line
+import { GraphQLAPI, GraphQLResult } from '@aws-amplify/api-graphql'; // eslint-disable-line
 import { Observable, Subscriber } from 'rxjs'; // eslint-disable-line
 import { Amplify } from 'aws-amplify';
 
@@ -18,12 +18,6 @@ export function mockApiResponse<T>(client: T, value: any) {
   return jest.spyOn(client as any, 'graphql').mockImplementation(async () => {
     return value;
   });
-}
-
-export interface GraphQLResult<T = object | null> {
-  data: T;
-  errors?: GraphQLError[];
-  extensions?: Record<string, any>;
 }
 
 /**
