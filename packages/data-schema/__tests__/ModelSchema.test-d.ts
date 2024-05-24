3438c82e8339748436c69f23ddcc435b22ea87f3
David McAfee <mcafd@amazon.com>
2024-05-24 16:49:31 -0700

    finish tests

--- packages/data-schema/__tests__/ModelSchema.test-d.ts
+++ packages/data-schema/__tests__/ModelSchema.test-d.ts
@@ -4,8 +4,6 @@ import { schema } from '../src/ModelSchema';
 import { configure } from '../src/internals';
 import { a } from '../src/index';
 
-type GetModelTypeArg<T> = T extends ModelType<infer R, any> ? R : never;
-
 const fakeSecret = () => ({}) as any;
 
 const datasourceConfigMySQL = {
@@ -37,85 +35,41 @@ describe('ModelSchema', () => {
       });
     });
   });
-  describe('[TODO] SQL', () => {
-    it('Runtime error when calling unsupported `setAuthorization` [TODO]', () => {
+  describe('SQL schema unsupported modifiers', () => {
+    it('expected errors for unsupported `setAuthorization` modifiers', () => {
       const sqlSchema = configure({ database: datasourceConfigMySQL })
         .schema({
-          post: a
-            .model({
-              id: a.string().required(),
-              title: a.string(),
-              author: a.string(),
-            })
-            .identifier(['id']),
-          comment: a
-            .model({
-              id: a.string().required(),
-              title: a.string(),
-              author: a.string(),
-            })
-            .identifier(['id']),
-          tags: a
-            .model({
-              id: a.string().required(),
-              title: a.string(),
-              author: a.string(),
-            })
-            .identifier(['id']),
+          post: a.model({
+            id: a.string().required(),
+            title: a.string(),
+          }),
         })
         .setAuthorization((models) => [
-          models.post.authorization((allow) => allow.publicApiKey()),
-          models.comment.authorization((allow) => allow.publicApiKey()),
-          models.tags.authorization((allow) => allow.publicApiKey()),
-          // For setAuthorization, the model shouldn't have "addRelationship" and "secondaryIndexes"
-          // Note: unable to repro `addRelationship`, may have been inadvertently fixed previously:
-          // @ts-expect-error
-          models.tags.secondaryIndexes((index) => [index('title')]),
+          // @ts-expect-error - not supported
+          models.post.secondaryIndexes((index) => [index('title')]),
+          // @ts-expect-error - not supported
+          models.post.addRelationship,
         ]);
     });
-    it('Runtime error when calling unsupported relationships [TODO]', () => {
+    it('expected errors for unsupported `setRelationship` modifiers', () => {
       const sqlSchema = configure({ database: datasourceConfigMySQL })
         .schema({
-          post: a
-            .model({
-              id: a.string().required(),
-              title: a.string(),
-              author: a.string(),
-            })
-            .identifier(['id']),
-          comment: a
-            .model({
-              id: a.string().required(),
-              title: a.string(),
-              author: a.string(),
-            })
-            .identifier(['id']),
-          tags: a
-            .model({
-              id: a.string().required(),
-              title: a.string(),
-              author: a.string(),
-            })
-            .identifier(['id']),
+          post: a.model({
+            id: a.string().required(),
+            title: a.string(),
+            author: a.string(),
+          }),
         })
         .setAuthorization((models) => [
           models.post.authorization((allow) => allow.publicApiKey()),
-          models.comment.authorization((allow) => allow.publicApiKey()),
-          models.tags.authorization((allow) => allow.publicApiKey()),
         ])
         .setRelationships((models) => [
-          models.post.relationships({
-            comments: a.hasMany('Comment', 'post_id'),
-          }),
-          models.comment.relationships({
-            note: a.belongsTo('Post', 'post_id'),
-          }),
-          // @ts-expect-error
-          models.tags.secondaryIndexes((index) => [index('title')]),
-          // @ts-expect-error
-          models.tags.authorization((allow) => allow.publicApiKey()),
-          // @ts-expect-error
-          models.tags.fields.title.authorization((allow) =>
+          // @ts-expect-error - not supported
+          models.post.secondaryIndexes((index) => [index('title')]),
+          // @ts-expect-error - not supported
+          models.post.authorization((allow) => allow.publicApiKey()),
+          // @ts-expect-error - not supported
+          models.post.fields.title.authorization((allow) =>
             allow.publicApiKey(),
           ),
         ]);
