8c5c576b451a43e47655d11c982afa479375a1cd
David McAfee <mcafd@amazon.com>
2024-05-24 16:15:10 -0700

    fix tests

--- packages/data-schema/__tests__/ModelSchema.test-d.ts
+++ packages/data-schema/__tests__/ModelSchema.test-d.ts
@@ -1,17 +1,8 @@
-import { type ModelType, type InternalModel, model } from '../src/ModelType';
-import {
-  type ModelField,
-  type InternalField,
-  string,
-  id,
-} from '../src/ModelField';
-import {
-  type ModelSchema,
-  type InternalSchema,
-  schema,
-} from '../src/ModelSchema';
+import { type ModelType, model } from '../src/ModelType';
+import { string, id } from '../src/ModelField';
+import { schema } from '../src/ModelSchema';
 import { configure } from '../src/internals';
-import { a, ClientSchema } from '../src/index';
+import { a } from '../src/index';
 
 type GetModelTypeArg<T> = T extends ModelType<infer R, any> ? R : never;
 
@@ -47,92 +38,87 @@ describe('ModelSchema', () => {
     });
   });
   describe('[TODO] SQL', () => {
-    test('Runtime error when calling unsupported `setAuthorization` [TODO]', () => {
-      expect(() => {
-        const sqlSchema = configure({ database: datasourceConfigMySQL })
-          .schema({
-            post: a
-              .model({
-                id: a.string().required(),
-                title: a.string(),
-                author: a.string(),
-              })
-              .identifier(['id']),
-            comment: a
-              .model({
-                id: a.string().required(),
-                title: a.string(),
-                author: a.string(),
-              })
-              .identifier(['id']),
-            tags: a
-              .model({
-                id: a.string().required(),
-                title: a.string(),
-                author: a.string(),
-              })
-              .identifier(['id']),
-          })
-          .setAuthorization((models) => [
-            models.post.authorization((allow) => allow.publicApiKey()),
-            models.comment.authorization((allow) => allow.publicApiKey()),
-            models.tags.authorization((allow) => allow.publicApiKey()),
-            // For setAuthorization, the model shouldn't have "addRelationship" and "secondaryIndexes"
-            // Note: unable to repro `addRelationship`, may have been inadvertently fixed previously:
-            // @ts-expect-error
-            models.tags.secondaryIndexes,
-          ]);
-      }).toThrowError();
+    it('Runtime error when calling unsupported `setAuthorization` [TODO]', () => {
+      const sqlSchema = configure({ database: datasourceConfigMySQL })
+        .schema({
+          post: a
+            .model({
+              id: a.string().required(),
+              title: a.string(),
+              author: a.string(),
+            })
+            .identifier(['id']),
+          comment: a
+            .model({
+              id: a.string().required(),
+              title: a.string(),
+              author: a.string(),
+            })
+            .identifier(['id']),
+          tags: a
+            .model({
+              id: a.string().required(),
+              title: a.string(),
+              author: a.string(),
+            })
+            .identifier(['id']),
+        })
+        .setAuthorization((models) => [
+          models.post.authorization((allow) => allow.publicApiKey()),
+          models.comment.authorization((allow) => allow.publicApiKey()),
+          models.tags.authorization((allow) => allow.publicApiKey()),
+          // For setAuthorization, the model shouldn't have "addRelationship" and "secondaryIndexes"
+          // Note: unable to repro `addRelationship`, may have been inadvertently fixed previously:
+          // @ts-expect-error
+          models.tags.secondaryIndexes((index) => [index('title')]),
+        ]);
     });
-    test('Runtime error when calling unsupported relationships [TODO]', () => {
-      expect(() => {
-        const sqlSchema = configure({ database: datasourceConfigMySQL })
-          .schema({
-            post: a
-              .model({
-                id: a.string().required(),
-                title: a.string(),
-                author: a.string(),
-              })
-              .identifier(['id']),
-            comment: a
-              .model({
-                id: a.string().required(),
-                title: a.string(),
-                author: a.string(),
-              })
-              .identifier(['id']),
-            tags: a
-              .model({
-                id: a.string().required(),
-                title: a.string(),
-                author: a.string(),
-              })
-              .identifier(['id']),
-          })
-          .setAuthorization((models) => [
-            models.post.authorization((allow) => allow.publicApiKey()),
-            models.comment.authorization((allow) => allow.publicApiKey()),
-            models.tags.authorization((allow) => allow.publicApiKey()),
-            // This shouldn't work:
-            // @ts-expect-error
-            models.tags.secondaryIndexes((index) => [index('title')]),
-          ])
-          .setRelationships((models) => [
-            models.post.relationships({
-              comments: a.hasMany('Comment', 'post_id'),
-            }),
-            models.comment.relationships({
-              note: a.belongsTo('Post', 'post_id'),
-            }),
-            // For relationships, the model shouldn't have "authorization", "secondaryIndexes", "fields":
-            // TODO: errors, but is still shown as option - test elsewhere
-            // models.tags.relationships({
-            //   note: a.belongsTo('Post', 'post_id'),
-            // }),
-            // models.tags
-          ]);
-      }).not.toThrowError();
+    it('Runtime error when calling unsupported relationships [TODO]', () => {
+      const sqlSchema = configure({ database: datasourceConfigMySQL })
+        .schema({
+          post: a
+            .model({
+              id: a.string().required(),
+              title: a.string(),
+              author: a.string(),
+            })
+            .identifier(['id']),
+          comment: a
+            .model({
+              id: a.string().required(),
+              title: a.string(),
+              author: a.string(),
+            })
+            .identifier(['id']),
+          tags: a
+            .model({
+              id: a.string().required(),
+              title: a.string(),
+              author: a.string(),
+            })
+            .identifier(['id']),
+        })
+        .setAuthorization((models) => [
+          models.post.authorization((allow) => allow.publicApiKey()),
+          models.comment.authorization((allow) => allow.publicApiKey()),
+          models.tags.authorization((allow) => allow.publicApiKey()),
+        ])
+        .setRelationships((models) => [
+          models.post.relationships({
+            comments: a.hasMany('Comment', 'post_id'),
+          }),
+          models.comment.relationships({
+            note: a.belongsTo('Post', 'post_id'),
+          }),
+          // @ts-expect-error
+          models.tags.secondaryIndexes((index) => [index('title')]),
+          // @ts-expect-error
+          models.tags.authorization((allow) => allow.publicApiKey()),
+          // @ts-expect-error
+          models.tags.fields.title.authorization((allow) =>
+            allow.publicApiKey(),
+          ),
+        ]);
     });
   });
 });
