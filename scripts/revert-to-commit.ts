9cd790014c34936519613032adb074c55cc41224
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2024-06-18 08:12:36 -0400

    filter out release commits

--- scripts/revert-to-commit.ts
+++ scripts/revert-to-commit.ts
@@ -1,13 +1,41 @@
 import { execa } from 'execa';
 
+const RELEASE_COMMIT_MESSAGE = 'release: publish [ci skip]';
+
+const ignoreReleaseCommits = async (commitId: string) => {
+  const { stdout: message } = await execa('git', [
+    'log',
+    '--format=%B',
+    '-n',
+    '1',
+    commitId,
+  ]);
+  // Don't revert release commits; this would remove existing library versions from the Changelog
+  return !message.includes(RELEASE_COMMIT_MESSAGE);
+};
+
 export async function revertToCommit(commitId: string) {
   try {
     // Create a new branch from the current branch
     const newBranchName = `revert-to-${commitId}`;
     await execa('git', ['checkout', '-b', newBranchName]);
 
-    // Revert to specified commit and commit to branch
-    await execa('git', ['revert', '--no-commit', `${commitId}..HEAD`]);
+    // Get the list of commits more recent than the provided commit id
+    const { stdout: commits } = await execa('git', [
+      'rev-list',
+      `${commitId}..HEAD`,
+    ]);
+
+    const commitList = await Promise.all(
+      commits.split('\n').reverse().filter(ignoreReleaseCommits),
+    );
+
+    // Revert each commit individually
+    for (const commit of commitList) {
+      await execa('git', ['revert', '--no-commit', commit]);
+    }
+
+    // Commit the reverts
     await execa('git', ['commit', '-m', `Revert to commit ${commitId}`]);
 
     console.log(
@@ -18,5 +46,5 @@ export async function revertToCommit(commitId: string) {
   }
 }
 
-const COMMIT_ID = '54b64f2424d7c1a2b5b7feb23c8453db0217d36d';
+const COMMIT_ID = '702d2160e61b6a50701d92e83821504c9c63f788';
 revertToCommit(COMMIT_ID);
