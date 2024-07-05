4dee0287fbab2194b51400af57168c3132a11cec
David McAfee <mcafd@amazon.com>
2024-07-05 16:50:33 -0700

    test2

--- packages/data-schema/src/runtime/internals/operations/get.ts
+++ packages/data-schema/src/runtime/internals/operations/get.ts
@@ -92,101 +92,101 @@ async function _get(
     modelIntrospection,
   );
 
-  const auth = authModeParams(client, getInternals, options);
+  // const auth = authModeParams(client, getInternals, options);
 
-  try {
-    const headers = getCustomHeaders(client, getInternals, options?.headers);
+  // try {
+  //   const headers = getCustomHeaders(client, getInternals, options?.headers);
 
-    const { data, extensions } = context
-      ? ((await (client as BaseSSRClient).graphql(
-          context,
-          {
-            ...auth,
-            query,
-            variables,
-          },
-          headers,
-        )) as GraphQLResult)
-      : ((await (client as BaseBrowserClient).graphql(
-          {
-            ...auth,
-            query,
-            variables,
-          },
-          headers,
-        )) as GraphQLResult);
+  //   const { data, extensions } = context
+  //     ? ((await (client as BaseSSRClient).graphql(
+  //         context,
+  //         {
+  //           ...auth,
+  //           query,
+  //           variables,
+  //         },
+  //         headers,
+  //       )) as GraphQLResult)
+  //     : ((await (client as BaseBrowserClient).graphql(
+  //         {
+  //           ...auth,
+  //           query,
+  //           variables,
+  //         },
+  //         headers,
+  //       )) as GraphQLResult);
 
-    // flatten response
-    if (data) {
-      const [key] = Object.keys(data);
-      const flattenedResult = flattenItems(modelIntrospection, name, data[key]);
+  //   // flatten response
+  //   if (data) {
+  //     const [key] = Object.keys(data);
+  //     const flattenedResult = flattenItems(modelIntrospection, name, data[key]);
 
-      if (flattenedResult === null) {
-        return { data: null, extensions };
-      } else if (options?.selectionSet) {
-        return { data: flattenedResult, extensions };
-      } else {
-        // TODO: refactor to avoid destructuring here
-        const [initialized] = initializeModel(
-          client,
-          name,
-          [flattenedResult],
-          modelIntrospection,
-          auth.authMode,
-          auth.authToken,
-          !!context,
-        );
+  //     if (flattenedResult === null) {
+  //       return { data: null, extensions };
+  //     } else if (options?.selectionSet) {
+  //       return { data: flattenedResult, extensions };
+  //     } else {
+  //       // TODO: refactor to avoid destructuring here
+  //       const [initialized] = initializeModel(
+  //         client,
+  //         name,
+  //         [flattenedResult],
+  //         modelIntrospection,
+  //         auth.authMode,
+  //         auth.authToken,
+  //         !!context,
+  //       );
 
-        return { data: initialized, extensions };
-      }
-    } else {
-      return { data: null, extensions };
-    }
-  } catch (error: any) {
-    /**
-     * The `data` type returned by `error` here could be:
-     * 1) `null`
-     * 2) an empty object
-     * 3) "populated" but with a `null` value `{ getPost: null }`
-     * 4) an actual record `{ getPost: { id: '1', title: 'Hello, World!' } }`
-     */
-    const { data, errors } = error;
+  //       return { data: initialized, extensions };
+  //     }
+  //   } else {
+  //     return { data: null, extensions };
+  //   }
+  // } catch (error: any) {
+  //   /**
+  //    * The `data` type returned by `error` here could be:
+  //    * 1) `null`
+  //    * 2) an empty object
+  //    * 3) "populated" but with a `null` value `{ getPost: null }`
+  //    * 4) an actual record `{ getPost: { id: '1', title: 'Hello, World!' } }`
+  //    */
+  //   const { data, errors } = error;
 
-    /**
-     * `data` is not `null`, and is not an empty object:
-     */
-    if (data && Object.keys(data).length !== 0 && errors) {
-      const [key] = Object.keys(data);
-      const flattenedResult = flattenItems(modelIntrospection, name, data[key]);
+  //   /**
+  //    * `data` is not `null`, and is not an empty object:
+  //    */
+  //   if (data && Object.keys(data).length !== 0 && errors) {
+  //     const [key] = Object.keys(data);
+  //     const flattenedResult = flattenItems(modelIntrospection, name, data[key]);
 
-      /**
-       * `flattenedResult` could be `null` here (e.g. `data: { getPost: null }`)
-       * if `flattenedResult`, result is an actual record:
-       */
-      if (flattenedResult) {
-        if (options?.selectionSet) {
-          return { data: flattenedResult, errors };
-        } else {
-          // TODO: refactor to avoid destructuring here
-          const [initialized] = initializeModel(
-            client,
-            name,
-            [flattenedResult],
-            modelIntrospection,
-            auth.authMode,
-            auth.authToken,
-            !!context,
-          );
+  //     /**
+  //      * `flattenedResult` could be `null` here (e.g. `data: { getPost: null }`)
+  //      * if `flattenedResult`, result is an actual record:
+  //      */
+  //     if (flattenedResult) {
+  //       if (options?.selectionSet) {
+  //         return { data: flattenedResult, errors };
+  //       } else {
+  //         // TODO: refactor to avoid destructuring here
+  //         const [initialized] = initializeModel(
+  //           client,
+  //           name,
+  //           [flattenedResult],
+  //           modelIntrospection,
+  //           auth.authMode,
+  //           auth.authToken,
+  //           !!context,
+  //         );
 
-          return { data: initialized, errors };
-        }
-      } else {
-        // was `data: { getPost: null }`)
-        return handleSingularGraphQlError(error);
-      }
-    } else {
-      // `data` is `null`:
-      return handleSingularGraphQlError(error);
-    }
-  }
+  //         return { data: initialized, errors };
+  //       }
+  //     } else {
+  //       // was `data: { getPost: null }`)
+  //       return handleSingularGraphQlError(error);
+  //     }
+  //   } else {
+  //     // `data` is `null`:
+  //     return handleSingularGraphQlError(error);
+  //   }
+  // }
 }
