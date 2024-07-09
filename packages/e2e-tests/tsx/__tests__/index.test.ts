33080e3102ceab6ea551665a7937ece56d922847
verdaccio <verdaccio@amplifyjs.com>
2024-07-08 22:39:22 -0700

    test and sample updates

--- packages/e2e-tests/tsx/__tests__/index.test.ts
+++ packages/e2e-tests/tsx/__tests__/index.test.ts
@@ -1,8 +1,15 @@
-import test1 from '../index';
+import testCreate from '../index';
 
 // TODO: test for each CRUD operation:
 
-test('todo', async () => {
-  const [result] = await test1();
-  expect(result.content).toBe('test content');
+describe('API CRUD operations', () => {
+  test('create', async () => {
+    const result = await testCreate();
+    // TODO:
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    expect(result.content).toBe('test content');
+  });
 });
+
+//  process.exit(1);
--- packages/e2e-tests/tsx/index.ts
+++ packages/e2e-tests/tsx/index.ts
@@ -3,33 +3,30 @@ import { generateClient } from 'aws-amplify/data';
 import type { Schema } from './amplify/data/resource';
 import outputs from './amplify_outputs.json';
 
-export async function test1() {
-  Amplify.configure(outputs);
-
-  const client = generateClient<Schema>();
-
-  const { data: todo, errors: createErrors } = await client.models.Todo.create({
-    content: 'test content',
-  });
-
-  console.log('created todo:', todo);
-  console.log('create errors:', createErrors);
-
-  console.log('delete all');
-
-  const { data: todos, errors } = await client.models.Todo.list();
-  console.log(errors);
-
-  console.log('starting delete for', todos);
+async function cleanup(client: any) {
+  // const { data: todos, errors } = await client.models.Todo.list();
+  const { data: todos } = await client.models.Todo.list();
 
   const deletePromises = todos?.map(async (todo: Schema['Todo']['type']) => {
     await client.models.Todo.delete(todo);
   });
 
   await Promise.all(deletePromises!);
-
-  return todos;
-  // process.exit(1);
 }
 
-export default test1;
+export async function testCreate() {
+  Amplify.configure(outputs);
+
+  const client = generateClient<Schema>();
+
+  const { data: newTodo, errors: createErrors } =
+    await client.models.Todo.create({
+      content: 'test content',
+    });
+
+  await cleanup(client);
+
+  return createErrors || newTodo;
+}
+
+export default testCreate;
