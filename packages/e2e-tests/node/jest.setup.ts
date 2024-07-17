3475e9252b42d1f79f894b37885d557f6f17aeb0
David McAfee <mcafd@amazon.com>
2024-07-17 11:07:47 -0700

    address PR feedback

--- packages/e2e-tests/node/jest.setup.ts
+++
@@ -1,7 +0,0 @@
-// TODO: open question
-// import { configureAmplifyAndGenerateClient } from './utils';
-
-// (async function () {
-//   const client = await configureAmplifyAndGenerateClient();
-//   (global as any).client = client;
-// })();
