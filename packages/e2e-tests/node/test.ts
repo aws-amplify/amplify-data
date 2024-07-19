0c4912101a0104894fc099ae6d30b1d3379b37e2
David McAfee <mcafd@amazon.com>
2024-07-19 15:06:22 -0700

    checkpoint

--- packages/e2e-tests/node/test.ts
+++ packages/e2e-tests/node/test.ts
@@ -1,23 +1,52 @@
 import { configureAmplifyAndGenerateClient, pause } from './utils';
 // import type { Schema } from '../amplify/data/resource';
 // import { CONNECTION_STATE_CHANGE, ConnectionState } from 'aws-amplify/data';
-import { Hub } from 'aws-amplify/utils';
+import { Hub, ConsoleLogger } from 'aws-amplify/utils';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { WebSocket } from 'ws';
+(global as any).WebSocket = WebSocket;
+
+ConsoleLogger.LOG_LEVEL = 'DEBUG';
 
 const client = await configureAmplifyAndGenerateClient();
 
-await Hub.listen('api', (data: any) => {
+// const wss = new WebSocketServer({ port: 8080 });
+// console.log(wss);
+
+// await wss.on('connection', function connection(ws: any) {
+//   console.log('new client connected');
+//   // sending message to client
+//   ws.send('Welcome, you are connected!');
+//   //on message from client
+//   ws.on('message', (data: any) => {
+//     console.log(`Client has sent us: ${data}`);
+//   });
+//   // handling what to do when clients disconnects from server
+//   ws.on('close', () => {
+//     console.log('the client has connected');
+//   });
+//   // handling client connection error
+//   ws.onerror = function () {
+//     console.log('Some Error occurred');
+//   };
+//   console.log('something is happening in ws--------');
+//   ws.send('something ws------------------');
+//   ws.send('something ws------------------');
+//   ws.send('something ws------------------');
+// });
+
+Hub.listen('api', (data: any) => {
   console.log('all hub data----------------', data);
-  // const { payload } = data;
-  // if (payload.event === CONNECTION_STATE_CHANGE) {
-  //   const connectionState = payload.data
-  //     .connectionState as ConnectionState;
-  //   console.log('connectoin stuff---------', connectionState);
-  // }
+});
+
+Hub.listen('auth', (data) => {
+  console.log('auth stuff------------------', data);
 });
 
 await client.models.Todo.onCreate().subscribe({
-  next: (data) => console.log('sub result-------------', data),
-  error: (error) => console.log('sub error----------------', error),
+  next: (data) => console.log('sub result-----------------', data),
+  error: (error) => console.log('sub error-------------------', error),
 });
 
 console.log('before pause');
@@ -30,6 +59,7 @@ const { data: newTodo, errors: createErrors } = await client.models.Todo.create(
   },
 );
 
+console.log('RESULT OF CREATE--------------------------------------');
 console.log('new todo', newTodo);
 console.log('create errors', createErrors);
 
@@ -42,6 +72,7 @@ const { data: newTodo2, errors: createErrors2 } =
     content: 'test create',
   });
 
+console.log('RESULT OF CREATE--------------------------------------');
 console.log('new todo', newTodo2);
 console.log('create errors', createErrors2);
 
@@ -54,6 +85,7 @@ const { data: newTodo3, errors: createErrors3 } =
     content: 'test create',
   });
 
+console.log('RESULT OF CREATE--------------------------------------');
 console.log('new todo', newTodo3);
 console.log('create errors', createErrors3);
 
@@ -66,6 +98,7 @@ const { data: newTodo4, errors: createErrors4 } =
     content: 'test create',
   });
 
+console.log('RESULT OF CREATE--------------------------------------');
 console.log('new todo', newTodo4);
 console.log('create errors', createErrors4);
 
