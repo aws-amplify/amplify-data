873b5951dbc431fdbea0eae2b39d0f93c30c3150
David McAfee <mcafd@amazon.com>
2024-07-19 11:36:17 -0700

    more testing

--- packages/e2e-tests/node/test.ts
+++ packages/e2e-tests/node/test.ts
@@ -16,10 +16,59 @@ await Hub.listen('api', (data: any) => {
 });
 
 await client.models.Todo.onCreate().subscribe({
-  next: (data) => console.log('sub result', data),
-  error: (error) => console.log('sub error', error),
+  next: (data) => console.log('sub result-------------', data),
+  error: (error) => console.log('sub error----------------', error),
 });
 
 console.log('before pause');
-await pause(50000);
+await pause(2000);
+console.log('after pause');
+
+const { data: newTodo, errors: createErrors } = await client.models.Todo.create(
+  {
+    content: 'test create',
+  },
+);
+
+console.log('new todo', newTodo);
+console.log('create errors', createErrors);
+
+console.log('before pause');
+await pause(3000);
+console.log('after pause');
+
+const { data: newTodo2, errors: createErrors2 } =
+  await client.models.Todo.create({
+    content: 'test create',
+  });
+
+console.log('new todo', newTodo2);
+console.log('create errors', createErrors2);
+
+console.log('before pause');
+await pause(5000);
+console.log('after pause');
+
+const { data: newTodo3, errors: createErrors3 } =
+  await client.models.Todo.create({
+    content: 'test create',
+  });
+
+console.log('new todo', newTodo3);
+console.log('create errors', createErrors3);
+
+console.log('before pause');
+await pause(10000);
+console.log('after pause');
+
+const { data: newTodo4, errors: createErrors4 } =
+  await client.models.Todo.create({
+    content: 'test create',
+  });
+
+console.log('new todo', newTodo4);
+console.log('create errors', createErrors4);
+
+console.log('before pause');
+await pause(10000);
 console.log('after pause');
