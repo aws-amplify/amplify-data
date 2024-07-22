26e4a660c4ff0c7e7771a468084c599a14c1c515
David McAfee <mcafd@amazon.com>
2024-07-22 11:12:43 -0700

    enable debug logging

--- packages/e2e-tests/node/__tests__/basic-crudl.ts
+++ packages/e2e-tests/node/__tests__/basic-crudl.ts
@@ -19,7 +19,7 @@ const deleteAll = async (client: Client) => {
 
 describe('Basic CRUDL', () => {
   beforeEach(() => {
-    client = configureAmplifyAndGenerateClient({ disableDebugLogging: true });
+    client = configureAmplifyAndGenerateClient({});
   });
   afterEach(async () => {
     await deleteAll(client);
