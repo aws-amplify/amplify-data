056beebdae2fa87412fc1509a865b5b75f8b4459
Jon Wire <iambipedal@gmail.com>
2024-08-21 17:40:03 -0500

    add docstring to selfAwareAsync util

--- packages/data-schema/src/runtime/utils/selfAwareAsync.ts
+++ packages/data-schema/src/runtime/utils/selfAwareAsync.ts
@@ -1,4 +1,17 @@
 /**
+ * Executes an `async` resolver function, providing the `Promise`-to-be-returned as the
+ * first argument to the resolver so that the resolver can refer to the `Promise` that
+ * external callers will see.
+ *
+ * ```ts
+ * const outer = selfAwareAsync(async inner => {
+ *  console.log(outer === inner); // true
+ * });
+ * ```
+ *
+ * This utility exists to reduce boilerplate in cases where promise resolving code needs
+ * to track or register its "own" `Promise` *as seen by the caller* in some way. E.g.,
+ * when mapping `Promise` chains for `client.cancel()`.
  *
  * @param resolver
  * @returns
