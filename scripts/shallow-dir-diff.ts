dd2d94ba1efdaedf923f11b931d70c08f0900c21
Kamil Sobol <sobkamil@amazon.com>
2024-10-21 10:06:44 -0700

    why??

--- scripts/shallow-dir-diff.ts
+++ scripts/shallow-dir-diff.ts
@@ -21,6 +21,9 @@ function main(a: string, b: string) {
       const aData = fs.readFileSync(`${aPath}`).toString();
       const bData = fs.readFileSync(`${bPath}`).toString();
       if (aData !== bData)
+        console.log(fileName);
+        console.log(aData);
+        console.log(bData);
         differences.push(`File contents differ for ${fileName}`);
     }
   }
