ef33ce85a814985cdf7b801957ba060d8133c136
Peter Vaiciulis <petesv@amazon.com>
2024-10-23 10:35:06 -0700

    test: errors on incorrect use of .default()

--- packages/integration-tests/__tests__/defined-behavior/2-expected-use/schema-errors.ts
+++ packages/integration-tests/__tests__/defined-behavior/2-expected-use/schema-errors.ts
@@ -16,12 +16,12 @@ describe('validated on execution', () => {
 
 
   describe.each([
-	{ field: a.string() },
-	{ field: a.integer() },
-	{ field: a.float() },
-	{ field: a.datetime() },
-	{ field: a.timestamp() },
-  ])('Generated fields and identifiers', ({field}) => {
+	{ case: 'string', field: a.string(), generationSupported: false },
+	{ case: 'integer', field: a.integer(), generationSupported: true },
+	{ case: 'float', field: a.float(), generationSupported: false },
+	{ case: 'datetime', field: a.datetime(), generationSupported: false },
+	{ case: 'timestamp', field: a.timestamp(), generationSupported: false },
+  ])('Generated fields and identifiers: $case', ({field, generationSupported}) => {
 	/**
 	 * We allowed nullable identifiers to support SQL generated fields
 	 * e.g. Postgres SERIAL ... See PR: #373
@@ -41,5 +41,21 @@ describe('validated on execution', () => {
 
 	  expect(() => schema.transform()).toThrow('a nullable identifier must be generatable');
 	});
+
+	  test('Throws if you use `.default()` on unsupported field', () => {
+		const schema = a.schema({
+		  Model: a
+			.model({
+			  nullableFieldNoDefault: field.default(),
+			})
+			.authorization((allow) => allow.publicApiKey())
+		});
+
+		if (!generationSupported) {
+		  expect(() => schema.transform()).toThrow('db generation not supported for field');
+		} else {
+		  expect(() => schema.transform()).not.toThrow();
+		}
+	  })
   })
 });
