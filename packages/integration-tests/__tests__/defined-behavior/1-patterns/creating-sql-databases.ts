e1c3008c7f9d704dcbdab6524850195847f3e01f
Jon Wire <wirej@amazon.com>
2024-11-04 16:57:58 -0600

    found a builder pattern i like

--- packages/integration-tests/__tests__/defined-behavior/1-patterns/creating-sql-databases.ts
+++ packages/integration-tests/__tests__/defined-behavior/1-patterns/creating-sql-databases.ts
@@ -4,6 +4,7 @@ import type {
   SelectionSet,
   Expect,
   Equal,
+  Prettify,
 } from '@aws-amplify/data-schema-types';
 import {
   buildAmplifyConfig,
@@ -13,29 +14,37 @@ import {
   expectGraphqlMatches,
 } from '../../utils';
 
-type SetKey<T, Key extends keyof T, Value> = {
+type SetKey<T, Key, Value> = {
   [K in keyof T]: K extends Key ? Value : T[K];
+};
+
+type SetInternalKey<
+  T extends { [internal]: any },
+  Key,
+  Value,
+  OmitKey extends string = never,
+> = {
+  [K in keyof T as K extends OmitKey ? never : K]: K extends internal
+    ? SetKey<T[K], Key, Value>
+    : T[K];
 };
 
 const internal = Symbol('amplifyData');
 type internal = typeof internal;
 
 type SchemaDefinition = {
-  tables: Record<string, Built<TableDefinition>>;
+  tables: Record<string, { [internal]: TableDefinition }>;
 };
 
 type TableDefinition = {
-  fields: Record<string, Built<FieldDefinition>>;
+  fields: Record<string, { [internal]: FieldDefinition }>;
 };
 
 type FieldDefinition = {
-  type: FieldType;
-};
-
-type FieldType = {
   name: string;
   isArray: boolean;
   isRef: boolean;
+  isRequired: boolean;
 };
 
 type Built<T> = {
@@ -48,51 +57,77 @@ function built<const T>(definition: T): Built<T> {
   };
 }
 
-function builder<T>(of: T) {}
+function omit<T extends object, K extends string>(o: T, k: K): Omit<T, K> {
+  const cp = { ...o } as any;
+  delete cp[k];
+  return cp;
+}
 
-function array<const Self extends Built<FieldDefinition>>(
+function array<const Self extends { [internal]: object }>(
   this: Self,
-): Omit<Self, 'array'> {
-  const newBase = this[internal];
-  newBase.type.isArray = true;
-  const newSelf = {
-    ...this,
-    [internal]: newBase,
-  } as any;
-  delete newSelf.array;
-  return newSelf;
+): SetInternalKey<Self, 'isArray', true, 'array'> {
+  return omit(
+    {
+      ...this,
+      [internal]: {
+        ...this[internal],
+        isArray: true,
+      },
+    },
+    'array',
+  ) as any;
+}
+
+function required<const Self extends { [internal]: object }>(
+  this: Self,
+): SetInternalKey<Self, 'isRequired', true, 'required'> {
+  return omit(
+    {
+      ...this,
+      [internal]: {
+        ...this[internal],
+        isRequired: true,
+      },
+    },
+    'required',
+  ) as any;
 }
 
 const sql = {
-  schema<T extends SchemaDefinition>(def: T) {
-    return def;
+  schema<const T extends SchemaDefinition>(def: T): Prettify<T> {
+    return def as any;
   },
   table<const T extends TableDefinition['fields']>(fields: T) {
     return built({ fields });
   },
   field<const Name extends string>(name: Name) {
-    const base = {
-      type: { name, isRef: false, isArray: false },
-    };
     return {
-      [internal]: base,
+      [internal]: {
+        name,
+        isRef: false,
+        isArray: false,
+        isRequired: false,
+      } as const,
       array,
-    };
+      required,
+    } as const;
   },
   ref<const Name extends string>(name: Name) {
-    const base = {
-      type: { name, isRef: true, isArray: false },
-    };
     return {
-      [internal]: base,
-    };
+      [internal]: {
+        name,
+        isRef: true,
+        isArray: false,
+        isRequired: false,
+      } as const,
+    } as const;
   },
 };
 
 const schema = sql.schema({
   tables: {
     address: sql.table({
-      number: sql.field('number'),
+      number: sql.field('number').array().required(),
       street: sql.field('string'),
       city: sql.field('string'),
       state: sql.field('string'),
@@ -102,13 +137,13 @@ const schema = sql.schema({
       firstName: sql.field('string'),
       lastName: sql.field('number'),
       favoriteColors: sql.field('string').array(),
-      address: sql.ref('address'),
+      // address: sql.ref('address'),
     }),
   },
 });
 
-schema['tables']['customer'][internal].fields.favoriteColors[internal].type
-  .isArray;
+const typeTest =
+  schema['tables']['customer'][internal].fields.favoriteColors[internal];
 
 describe('creating sql databases', () => {
   const datasource = {};
