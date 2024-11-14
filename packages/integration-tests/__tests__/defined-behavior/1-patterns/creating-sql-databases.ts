9882cd38b83f46e8a91ebbb90cbba2da45fd48ab
Jon Wire <wirej@amazon.com>
2024-11-14 09:27:06 -0600

    increase sql schema test size, added some benchmarks

--- packages/integration-tests/__tests__/defined-behavior/1-patterns/creating-sql-databases.ts
+++ packages/integration-tests/__tests__/defined-behavior/1-patterns/creating-sql-databases.ts
@@ -17,7 +17,7 @@ import {
 
 const sqlSchema = a.sql.schema({
   tables: {
-    address: a.sql.table({
+    address1: a.sql.table({
       number: a.sql.int().array().required(),
       street: a.sql.varchar(),
       city: a.sql.varchar(),
@@ -26,7 +26,133 @@ const sqlSchema = a.sql.schema({
       lat: a.sql.real(),
       long: a.sql.real(),
     }),
-    customer: a.sql
+    customer1: a.sql
+      .table({
+        firstName: a.sql.varchar().required(),
+        lastName: a.sql.varchar().required(),
+        jobTitle: a.sql.varchar(50),
+        bio: a.sql.text(),
+        favoriteColors: a.sql.varchar().array(),
+      })
+      .identifier(['firstName', 'lastName']),
+    address2: a.sql.table({
+      number: a.sql.int().array().required(),
+      street: a.sql.varchar(),
+      city: a.sql.varchar(),
+      state: a.sql.varchar(),
+      zip: a.sql.varchar(),
+      lat: a.sql.real(),
+      long: a.sql.real(),
+    }),
+    customer2: a.sql
+      .table({
+        firstName: a.sql.varchar().required(),
+        lastName: a.sql.varchar().required(),
+        jobTitle: a.sql.varchar(50),
+        bio: a.sql.text(),
+        favoriteColors: a.sql.varchar().array(),
+      })
+      .identifier(['firstName', 'lastName']),
+    address3: a.sql.table({
+      number: a.sql.int().array().required(),
+      street: a.sql.varchar(),
+      city: a.sql.varchar(),
+      state: a.sql.varchar(),
+      zip: a.sql.varchar(),
+      lat: a.sql.real(),
+      long: a.sql.real(),
+    }),
+    customer3: a.sql
+      .table({
+        firstName: a.sql.varchar().required(),
+        lastName: a.sql.varchar().required(),
+        jobTitle: a.sql.varchar(50),
+        bio: a.sql.text(),
+        favoriteColors: a.sql.varchar().array(),
+      })
+      .identifier(['firstName', 'lastName']),
+    address4: a.sql.table({
+      number: a.sql.int().array().required(),
+      street: a.sql.varchar(),
+      city: a.sql.varchar(),
+      state: a.sql.varchar(),
+      zip: a.sql.varchar(),
+      lat: a.sql.real(),
+      long: a.sql.real(),
+    }),
+    customer4: a.sql
+      .table({
+        firstName: a.sql.varchar().required(),
+        lastName: a.sql.varchar().required(),
+        jobTitle: a.sql.varchar(50),
+        bio: a.sql.text(),
+        favoriteColors: a.sql.varchar().array(),
+      })
+      .identifier(['firstName', 'lastName']),
+    address5: a.sql.table({
+      number: a.sql.int().array().required(),
+      street: a.sql.varchar(),
+      city: a.sql.varchar(),
+      state: a.sql.varchar(),
+      zip: a.sql.varchar(),
+      lat: a.sql.real(),
+      long: a.sql.real(),
+    }),
+    customer5: a.sql
+      .table({
+        firstName: a.sql.varchar().required(),
+        lastName: a.sql.varchar().required(),
+        jobTitle: a.sql.varchar(50),
+        bio: a.sql.text(),
+        favoriteColors: a.sql.varchar().array(),
+      })
+      .identifier(['firstName', 'lastName']),
+    address6: a.sql.table({
+      number: a.sql.int().array().required(),
+      street: a.sql.varchar(),
+      city: a.sql.varchar(),
+      state: a.sql.varchar(),
+      zip: a.sql.varchar(),
+      lat: a.sql.real(),
+      long: a.sql.real(),
+    }),
+    customer6: a.sql
+      .table({
+        firstName: a.sql.varchar().required(),
+        lastName: a.sql.varchar().required(),
+        jobTitle: a.sql.varchar(50),
+        bio: a.sql.text(),
+        favoriteColors: a.sql.varchar().array(),
+      })
+      .identifier(['firstName', 'lastName']),
+    address7: a.sql.table({
+      number: a.sql.int().array().required(),
+      street: a.sql.varchar(),
+      city: a.sql.varchar(),
+      state: a.sql.varchar(),
+      zip: a.sql.varchar(),
+      lat: a.sql.real(),
+      long: a.sql.real(),
+    }),
+    customer7: a.sql
+      .table({
+        firstName: a.sql.varchar().required(),
+        lastName: a.sql.varchar().required(),
+        jobTitle: a.sql.varchar(50),
+        bio: a.sql.text(),
+        favoriteColors: a.sql.varchar().array(),
+      })
+      .identifier(['firstName', 'lastName']),
+    address8: a.sql.table({
+      number: a.sql.int().array().required(),
+      street: a.sql.varchar(),
+      city: a.sql.varchar(),
+      state: a.sql.varchar(),
+      zip: a.sql.varchar(),
+      lat: a.sql.real(),
+      long: a.sql.real(),
+    }),
+    customer8: a.sql
       .table({
         firstName: a.sql.varchar().required(),
         lastName: a.sql.varchar().required(),
@@ -40,10 +166,38 @@ const sqlSchema = a.sql.schema({
 
 const schema = a
   .schema({
-    Address: sqlSchema.tables.address
+    Address1: sqlSchema.tables.address1
       .toAPIModel()
       .authorization((allow) => [allow.owner(), allow.group('Admins')]),
-    Customer: sqlSchema.tables.customer.toAPIModel(),
+    Customer1: sqlSchema.tables.customer1.toAPIModel(),
+    Address2: sqlSchema.tables.address1
+      .toAPIModel()
+      .authorization((allow) => [allow.owner(), allow.group('Admins')]),
+    Customer2: sqlSchema.tables.customer1.toAPIModel(),
+    Address3: sqlSchema.tables.address1
+      .toAPIModel()
+      .authorization((allow) => [allow.owner(), allow.group('Admins')]),
+    Customer3: sqlSchema.tables.customer1.toAPIModel(),
+    Address4: sqlSchema.tables.address1
+      .toAPIModel()
+      .authorization((allow) => [allow.owner(), allow.group('Admins')]),
+    Customer4: sqlSchema.tables.customer1.toAPIModel(),
+    Address5: sqlSchema.tables.address1
+      .toAPIModel()
+      .authorization((allow) => [allow.owner(), allow.group('Admins')]),
+    Customer5: sqlSchema.tables.customer1.toAPIModel(),
+    Address6: sqlSchema.tables.address1
+      .toAPIModel()
+      .authorization((allow) => [allow.owner(), allow.group('Admins')]),
+    Customer6: sqlSchema.tables.customer1.toAPIModel(),
+    Address7: sqlSchema.tables.address1
+      .toAPIModel()
+      .authorization((allow) => [allow.owner(), allow.group('Admins')]),
+    Customer7: sqlSchema.tables.customer1.toAPIModel(),
+    Address8: sqlSchema.tables.address1
+      .toAPIModel()
+      .authorization((allow) => [allow.owner(), allow.group('Admins')]),
+    Customer8: sqlSchema.tables.customer1.toAPIModel(),
     NonSqlTable: a
       .model({
         a: a.string().required(),
@@ -55,6 +209,8 @@ const schema = a
   .authorization((allow) => allow.owner());
 
 type Schema = ClientSchema<typeof schema>;
+
+type TEST = Schema['Address3']['type'];
 
 describe('sql resource definitions', () => {
   beforeEach(async () => {
@@ -197,7 +353,7 @@ describe('sql resource definitions', () => {
     ]);
     const client = generateClient<Schema>();
 
-    const { data: created } = await client.models.Customer.create({
+    const { data: created } = await client.models.Customer1.create({
       firstName: 'First',
       lastName: 'Last',
       favoriteColors: ['red', 'green', 'blue'],
@@ -238,7 +394,7 @@ describe('sql resource definitions', () => {
     ]);
     const client = generateClient<Schema>();
 
-    const { data: gotten } = await client.models.Customer.get({
+    const { data: gotten } = await client.models.Customer1.get({
       firstName: 'First',
       lastName: 'Last',
     });
