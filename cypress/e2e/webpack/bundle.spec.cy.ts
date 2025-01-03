61c4eb1366f4e5d48104eaaf2fcf17127c1422b5
Jack Lu <lzhouq@amazon.com>
2025-01-03 15:38:22 -0800

    Modify test

--- cypress/e2e/webpack/bundle.spec.cy.ts
+++ cypress/e2e/webpack/bundle.spec.cy.ts
@@ -1,5 +1,8 @@
 describe('webpack spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000');
+    cy.request('/main.bundle.js').then((response) => {
+      cy.log('main.bundle.js content:', response.body);
+    });
   });
 });
