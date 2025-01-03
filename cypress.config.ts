2e83b1b61c246bed8136f06d24b53a92eeb29f7b
Jack Lu <lzhouq@amazon.com>
2025-01-03 10:37:44 -0800

    Modify Cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  includeShadowDom: true,
+  defaultCommandTimeout: 30000,
+  experimentalWebKitSupport: true,
+  experimentalModifyObstructiveThirdPartyCode: true,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000/',
+    specPattern: 'cypress/e2e/**/*.spec.cy.{ts,js}',
+    excludeSpecPattern: ['**/__snapshots__/*', '**/__image_snapshots__/*'],
   },
+  chromeWebSecurity: false,
 });
