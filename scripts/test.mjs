bb341bf70a9df901a0812103855bff872996c826
Jack Lu <lzhouq@amazon.com>
2025-01-15 23:18:53 -0800

    Test

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -121,7 +121,7 @@ const startSampleAndRun = async () => {
   // commands
   const runApp =
     build === BUILD_TYPE.dev ? runAppOnDev(params) : runAppOnProd(params);
-  const runTest = `wait-on -t ${defaultTimeout} ${waitOnOption} && curl -o /dev/null -s -w "%{http_code}" http://localhost:3000/main.bundle-t.js`;
+  const runTest = `wait-on -t ${defaultTimeout} ${waitOnOption} && curl -o /dev/null -s -w "%{http_code}" http://localhost:3000/main.bundle.js`;
 
   const { result } = concurrently([runApp, runTest], {
     killOthers: ['success', 'failure'],
