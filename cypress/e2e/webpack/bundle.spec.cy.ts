856994177d290095bbe33d36a7f12e38bd5adad6
Jack Lu <lzhouq@amazon.com>
2025-01-21 15:50:23 -0800

    Test cy.fail

--- cypress/e2e/webpack/bundle.spec.cy.ts
+++ cypress/e2e/webpack/bundle.spec.cy.ts
@@ -1,5 +1,6 @@
 describe('webpack spec', () => {
   it('passes', () => {
+    cy.fail('Intentionally failing the test');
     cy.visit('http://localhost:3001');
     cy.request('/main.bundle.js').then((response) => {
       cy.log('main.bundle.js content:', response.body);
