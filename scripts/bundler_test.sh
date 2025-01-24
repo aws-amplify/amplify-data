bc91709370e5c63bbf8735780050b79a30dc57ed
Jack Lu <lzhouq@amazon.com>
2025-01-23 16:38:39 -0800

    Test prod

--- scripts/bundler_test.sh
+++ scripts/bundler_test.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
 
-# Run the npm commands
-echo "dev npm --prefix packages/e2e-tests/webpack install && npm --prefix packages/e2e-tests/webpack run start"
-npm --prefix packages/e2e-tests/webpack install && npm --prefix packages/e2e-tests/webpack run start &
+# Global variables to store PIDs
+NPM_PID=""
 
-# Store the PID of the npm process
-NPM_PID=$!
+# Get the input argument
+ENV=$1
+
+# Function to run dev environment
+run_dev() {
+    echo "dev npm --prefix packages/e2e-tests/webpack install && npm --prefix packages/e2e-tests/webpack run start"
+    npm --prefix packages/e2e-tests/webpack install && npm --prefix packages/e2e-tests/webpack run start &
+    NPM_PID=$!
+}
+
+# Function to run prod environment
+run_prod() {
+    echo "npm --prefix packages/e2e-tests/webpack install && 
+    npm --prefix packages/e2e-tests/webpack run build && 
+    npm --prefix packages/e2e-tests/webpack run serve"
+    npm --prefix packages/e2e-tests/webpack install && 
+    npm --prefix packages/e2e-tests/webpack run build && 
+    npm --prefix packages/e2e-tests/webpack run serve &
+    NPM_PID=$!
+}
+
+# Check the input and run the appropriate command
+if [ "$ENV" = "dev" ]; then
+    run_dev
+elif [ "$ENV" = "prod" ]; then
+    run_prod
+else
+    echo "Invalid argument. Please use 'dev' or 'prod'."
+    exit 1
+fi
 
 # Function to check if the server is up
 echo "curl -s -o /dev/null -w "%{http_code}" http://localhost:3000 | grep -q "200""
@@ -21,7 +48,7 @@ check_server() {
 
 # Wait for the server to start (with timeout)
 if check_server; then
-    echo "Server is up and running!"
+    echo "Test pass!"
     # Kill the npm process
     kill $NPM_PID
     exit 0
