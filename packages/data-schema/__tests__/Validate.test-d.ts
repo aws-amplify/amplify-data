43b1dc7809d0d7a434007e81ac196326d90795c2
bobbyu99 <bobbyu@amazon.com>
2025-03-07 14:13:03 -0800

    feat: add validate builder function (#542)

---
+++ packages/data-schema/__tests__/Validate.test-d.ts
@@ -0,0 +1,247 @@
+import { a } from '../src';
+
+/**
+ * This test suite checks that the validate builder function is only available on 
+ * String, Integer, and Float field types, and not available on any other field types.
+ */
+describe('Validate builder function availability on different field types', () => {
+  it('Validate builder function is available on String, Integer, and Float field types', () => {
+    const stringField = a.string().validate(v => v.minLength(5));
+    const integerField = a.integer().validate(v => v.gt(10));
+    const floatField = a.float().validate(v => v.lt(100));
+  });
+  
+  it('Validate builder function is not available on any other field types', () => {
+    // @ts-expect-error
+    const idField = a.id().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const booleanField = a.boolean().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const dateField = a.date().validate(v => v.gt(10));
+    // @ts-expect-error
+    const timeField = a.time().validate(v => v.gt(10));
+    // @ts-expect-error
+    const dateTimeField = a.datetime().validate(v => v.gt(10));
+    // @ts-expect-error
+    const timestampField = a.timestamp().validate(v => v.endsWith('Z'));
+    // @ts-expect-error
+    const emailField = a.email().validate(v => v.lt(100));
+    // @ts-expect-error
+    const jsonField = a.json().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const phoneField = a.phone().validate(v => v.gt(10));
+    // @ts-expect-error
+    const urlField = a.url().validate(v => v.lt(100));
+    // @ts-expect-error
+    const ipAddressField = a.ipAddress().validate(v => v.gt(10));
+  });
+});
+
+/**
+ * This test suite checks that the validate builder function is not available on any array field types.
+ */
+describe('Validate builder function is not available on array field types', () => {
+  it('Validate builder function is not available on array field types', () => {
+    // @ts-expect-error
+    const stringArrayField = a.string().array().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const integerArrayField = a.integer().array().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const floatArrayField = a.float().array().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const idArrayField = a.id().array().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const booleanArrayField = a.boolean().array().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const dateArrayField = a.date().array().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const timeArrayField = a.time().array().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const dateTimeArrayField = a.datetime().array().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const timestampArrayField = a.timestamp().array().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const emailArrayField = a.email().array().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const jsonArrayField = a.json().array().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const phoneArrayField = a.phone().array().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const urlArrayField = a.url().array().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const ipAddressArrayField = a.ipAddress().array().validate(v => v.minLength(5));
+  });
+});
+
+/**
+ * This test suite checks that the validate builder function has to always go after the default builder function.
+ */
+describe('Validate builder function must go after the default builder function', () => {
+  it('Validate builder function after default builder function is acceptable', () => {
+    const stringField = a.string().default('test').validate(v => v.minLength(5));
+    const integerField = a.integer().default(10).validate(v => v.gt(10));
+    const floatField = a.float().default(100).validate(v => v.lt(100));
+  });
+
+  it('Validate builder function before default builder function is not acceptable', () => {
+    // @ts-expect-error
+    const stringField = a.string().validate(v => v.minLength(5)).default('test');
+    // @ts-expect-error
+    const integerField = a.integer().validate(v => v.gt(10)).default(10);
+    // @ts-expect-error
+    const floatField = a.float().validate(v => v.lt(100)).default(100);
+  });
+});
+
+/**
+ * This test suite checks the compatibility between field types and validators.
+ */
+describe('Validation compatibility between field types and validators', () => {
+  it('String field type with string validators are compatible', () => {
+    const stringField = a.string().validate(v => v
+      .minLength(5, 'String must be at least 5 characters long')
+      .maxLength(100, 'String must be at most 100 characters long')
+      .startsWith('prefix-', 'String must start with prefix-')
+      .endsWith('-suffix', 'String must end with -suffix')
+      .matches('^[a-zA-Z0-9]+$', 'String must be alphanumeric')
+    );
+  });
+
+  it('Integer field type with numeric validators are compatible', () => {
+    const integerField1 = a.integer().validate(v => v
+      .gt(10, 'Integer must be greater than 10')
+      .lt(100, 'Integer must be less than 100')
+      .gte(0, 'Integer must be greater than or equal to 0')
+      .lte(50, 'Integer must be less than or equal to 50')
+    );
+    const integerField2 = a.integer().validate(v => v.positive());
+    const integerField3 = a.integer().validate(v => v.negative());
+  });
+
+  it('Float field type with numeric validators are compatible', () => {
+    const floatField1 = a.float().validate(v => v
+      .gt(3.14, 'Float must be greater than 3.14')
+      .lt(9.99, 'Float must be less than 9.99')
+      .gte(0.0, 'Float must be greater than or equal to 0.0')
+      .lte(10.5, 'Float must be less than or equal to 10.5')
+    );
+    const floatField2 = a.float().validate(v => v.positive());
+    const floatField3 = a.float().validate(v => v.negative());
+  });
+
+  it('String field types with numeric validators are incompatible', () => {
+    // @ts-expect-error
+    const stringField1 = a.string().validate(v => v.gt(10));
+    // @ts-expect-error
+    const stringField2 = a.string().validate(v => v.lt(100));
+    // @ts-expect-error
+    const stringField3 = a.string().validate(v => v.gte(0));
+    // @ts-expect-error
+    const stringField4 = a.string().validate(v => v.lte(50));
+    // @ts-expect-error
+    const stringField5 = a.string().validate(v => v.positive());
+    // @ts-expect-error
+    const stringField6 = a.string().validate(v => v.negative());
+  });
+
+  it('Integer field type with string validators are incompatible', () => {
+    // @ts-expect-error
+    const integerField1 = a.integer().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const integerField2 = a.integer().validate(v => v.maxLength(100));
+    // @ts-expect-error
+    const integerField3 = a.integer().validate(v => v.startsWith('prefix-'));
+    // @ts-expect-error
+    const integerField4 = a.integer().validate(v => v.endsWith('-suffix'));
+    // @ts-expect-error
+    const integerField5 = a.integer().validate(v => v.matches('[0-9]+'));
+  });
+
+  it('Float field type with string validators are incompatible', () => {
+    // @ts-expect-error
+    const floatField1 = a.float().validate(v => v.minLength(5));
+    // @ts-expect-error
+    const floatField2 = a.float().validate(v => v.maxLength(100));
+    // @ts-expect-error
+    const floatField3 = a.float().validate(v => v.startsWith('prefix-'));
+    // @ts-expect-error
+    const floatField4 = a.float().validate(v => v.endsWith('-suffix'));
+    // @ts-expect-error
+    const floatField5 = a.float().validate(v => v.matches('[0-9]+'));
+  });
+});
+
+/**
+ * This test suite checks that positive and gt/negative and lt cannot be used together. 
+ * The reason is that they use the same operator (> or <), which is disallowed in the Validate Transformer.
+ */
+describe('Positive and gt/negative and lt cannot be used together', () => {
+  it('Positive and gt cannot be used together', () => {
+    // @ts-expect-error
+    const integerField = a.integer().validate(v => v.positive().gt(10));
+    // @ts-expect-error
+    const floatField = a.float().validate(v => v.gt(10).positive());
+  });
+
+  it('Negative and lt cannot be used together', () => {
+    // @ts-expect-error
+    const integerField = a.integer().validate(v => v.negative().lt(10));
+    // @ts-expect-error
+    const floatField = a.float().validate(v => v.lt(10).negative());
+  });
+
+  it('Positive, lt, gte, lte can be used together', () => {
+    const integerField = a.integer().validate(v => v.positive().lt(10).gte(0).lte(50));
+    const floatField = a.float().validate(v => v.positive().lt(10).gte(0).lte(50));
+  });
+
+  it('Negative, gt, gte, lte can be used together', () => {
+    const integerField = a.integer().validate(v => v.negative().gt(10).gte(0).lte(50));
+    const floatField = a.float().validate(v => v.negative().gt(10).gte(0).lte(50));
+  });
+});
+
+/**
+ * This test suite checks that each validator can only be used once on the same field.
+ */
+describe('Each validator can only be used once on the same field', () => {
+  it('Numeric validators can only be used once on the same Integer/Float field', () => {
+    // @ts-expect-error
+    const integerField1 = a.integer().validate(v => v.gt(0).gt(10));
+    // @ts-expect-error
+    const integerField2 = a.integer().validate(v => v.lt(0).lt(10));
+    // @ts-expect-error
+    const integerField3 = a.integer().validate(v => v.gte(0).gte(10));
+    // @ts-expect-error
+    const integerField4 = a.integer().validate(v => v.lte(0).lte(10));
+    // @ts-expect-error
+    const integerField5 = a.integer().validate(v => v.positive().positive());
+    // @ts-expect-error
+    const integerField6 = a.integer().validate(v => v.negative().negative());
+    // @ts-expect-error
+    const floatField1 = a.float().validate(v => v.gt(0).gt(10));
+    // @ts-expect-error
+    const floatField2 = a.float().validate(v => v.lt(0).lt(10));
+    // @ts-expect-error
+    const floatField3 = a.float().validate(v => v.gte(0).gte(10));
+    // @ts-expect-error
+    const floatField4 = a.float().validate(v => v.lte(0).lte(10));
+    // @ts-expect-error
+    const floatField5 = a.float().validate(v => v.positive().positive());
+    // @ts-expect-error
+    const floatField6 = a.float().validate(v => v.negative().negative());
+  });
+
+  it('String validators can only be used once on the same String field', () => {
+    // @ts-expect-error
+    const stringField1 = a.string().validate(v => v.minLength(5).minLength(10));
+    // @ts-expect-error
+    const stringField2 = a.string().validate(v => v.maxLength(5).maxLength(10));
+    // @ts-expect-error
+    const stringField3 = a.string().validate(v => v.startsWith('prefix-').startsWith('prefix-'));
+    // @ts-expect-error
+    const stringField4 = a.string().validate(v => v.endsWith('-suffix').endsWith('-suffix'));
+    // @ts-expect-error
+    const stringField5 = a.string().validate(v => v.matches('^[a-zA-Z0-9]+$').matches('^[a-zA-Z0-9]+$'));
+  });
+});
--- packages/data-schema/src/ModelField.ts
+++ packages/data-schema/src/ModelField.ts
@@ -2,6 +2,11 @@ import { brand } from './util';
 import { AllowModifier, Authorization, allow } from './Authorization';
 import type { methodKeyOf, satisfy } from './util/usedMethods.js';
 import type { brandSymbol } from './util/Brand.js';
+import {
+  ValidationRule,
+  FieldTypeToValidationBuilder,
+  createValidationBuilder,
+} from './Validate';
 
 /**
  * Used to "attach" auth types to ModelField without exposing them on the builder.
@@ -52,6 +57,7 @@ type FieldData = {
   arrayRequired: boolean;
   default: undefined | symbol | ModelFieldTypeParamOuter;
   authorization: Authorization<any, any, any>[];
+  validation: ValidationRule[];
 };
 
 type ModelFieldTypeParamInner = string | number | boolean | Date | Json | null;
@@ -79,11 +85,12 @@ export type ArrayField<T> = [T] extends [ModelFieldTypeParamInner]
 
 export type BaseModelField<
   T extends ModelFieldTypeParamOuter = ModelFieldTypeParamOuter,
-> = ModelField<T, UsableModelFieldKey, any>;
+  FT extends ModelFieldType = ModelFieldType
+> = ModelField<T, UsableModelFieldKey, any, FT>
 
 export type UsableModelFieldKey = satisfy<
   methodKeyOf<ModelField>,
-  'required' | 'default' | 'authorization' | 'array'
+  'required' | 'default' | 'authorization' | 'array' | 'validate'
 >;
 
 /**
@@ -92,11 +99,14 @@ export type UsableModelFieldKey = satisfy<
  *
  * @typeParam T - holds the JS data type of the field
  * @typeParam UsedMethod - union of strings representing already-invoked method names. Used to improve Intellisense
+ * @typeParam Auth - type of the authorization rules attached to the field
+ * @typeParam FT - specific ModelFieldType of the field
  */
 export type ModelField<
   T extends ModelFieldTypeParamOuter = ModelFieldTypeParamOuter,
   UsedMethod extends UsableModelFieldKey = never,
   Auth = undefined,
+  FT extends ModelFieldType = ModelFieldType
 > = Omit<
   {
     // This is a lie. This property is never set at runtime. It's just used to smuggle auth types through.
@@ -109,19 +119,21 @@ export type ModelField<
      * some property on the type is guaranteed to reference `T`
      * Context: https://github.com/aws-amplify/amplify-data/pull/406/files#r1869481467
      */
-    [internal](): ModelField<T>;
+    [internal](): ModelField<T, UsedMethod, Auth, FT>;
 
     /**
      * Marks a field as required.
      */
-    required(): ModelField<Required<T>, UsedMethod | 'required'>;
+    required(): ModelField<Required<T>, UsedMethod | 'required', Auth, FT>;
     // Exclude `optional` after calling array, because both the value and the array itself can be optional
     /**
      * Converts a field type definition to an array of the field type.
      */
     array(): ModelField<
       ArrayField<T>,
-      Exclude<UsedMethod, 'required'> | 'array'
+      Exclude<UsedMethod, 'required'> | 'array' | 'validate',
+      Auth,
+      FT
     >;
     // TODO: should be T, but .array breaks this constraint. Fix later
     /**
@@ -130,7 +142,7 @@ export type ModelField<
      */
     default(
       value?: ModelFieldTypeParamOuter,
-    ): ModelField<T, UsedMethod | 'default'>;
+    ): ModelField<T, UsedMethod | 'default', Auth, FT>;
     /**
      * Configures field-level authorization rules. Pass in an array of authorizations `(allow => allow.____)` to mix and match
      * multiple authorization rules for this field.
@@ -139,7 +151,13 @@ export type ModelField<
       callback: (
         allow: Omit<AllowModifier, 'resource'>,
       ) => AuthRuleType | AuthRuleType[],
-    ): ModelField<T, UsedMethod | 'authorization', AuthRuleType>;
+    ): ModelField<T, UsedMethod | 'authorization', AuthRuleType, FT>;
+    /**
+     * Configures field-level validation rules.
+     */
+    validate(
+      callback: (v: FieldTypeToValidationBuilder<T, FT>) => void
+    ): ModelField<T, UsedMethod | 'validate' | 'default' | 'array', Auth, FT>;
   },
   UsedMethod
 >;
@@ -148,7 +166,12 @@ export type ModelField<
  * Internal representation of Model Field that exposes the `data` property.
  * Used at buildtime.
  */
-export interface InternalField extends ModelField {
+export type InternalField<
+  T extends ModelFieldTypeParamOuter = ModelFieldTypeParamOuter,
+  UsedMethod extends UsableModelFieldKey = never,
+  Auth = undefined,
+  FT extends ModelFieldType = ModelFieldType
+> = ModelField<T, UsedMethod, Auth, FT> & {
   data: FieldData;
 }
 
@@ -164,7 +187,9 @@ export interface InternalField extends ModelField {
  *
  * @param fieldType - stores the GraphQL data type of the field
  */
-function _field<T extends ModelFieldTypeParamOuter>(fieldType: ModelFieldType) {
+function _field<T extends ModelFieldTypeParamOuter, FT extends ModelFieldType>(
+  fieldType: FT
+): ModelField<T, never, undefined, FT> {
   const _meta: FieldMeta = {
     lastInvokedMethod: null,
   };
@@ -176,6 +201,7 @@ function _field<T extends ModelFieldTypeParamOuter>(fieldType: ModelFieldType) {
     arrayRequired: false,
     default: undefined,
     authorization: [],
+    validation: [],
   };
 
   const builder = {
@@ -190,7 +216,7 @@ function _field<T extends ModelFieldTypeParamOuter>(fieldType: ModelFieldType) {
 
       return this;
     },
-    array(): ModelField<ArrayField<T>> {
+    array(): ModelField<ArrayField<T>, 'array', undefined, FT> {
       data.array = true;
       _meta.lastInvokedMethod = 'array';
 
@@ -210,15 +236,23 @@ function _field<T extends ModelFieldTypeParamOuter>(fieldType: ModelFieldType) {
 
       return this;
     },
+    validate(
+      callback: (v: FieldTypeToValidationBuilder<T, FT>) => void
+    ) {
+      const { builder, getRules } = createValidationBuilder<T, FT>();
+      callback(builder);
+      data.validation = getRules();
+      
+      _meta.lastInvokedMethod = 'validate';
+      
+      return this;
+    },
     ...brand(brandName),
     [internal]() {
       return this;
     },
-  } as ModelField<T>;
-
-  // this double cast gives us a Subtyping Constraint i.e., hides `data` from the public API,
-  // but makes it available internally when needed
-  return { ...builder, data } as InternalField as ModelField<T>;
+  } as ModelField<T, never, undefined, FT>;
+  return { ...builder, data } as InternalField as ModelField<T, never, undefined, FT>;
 }
 
 /**
@@ -226,80 +260,80 @@ function _field<T extends ModelFieldTypeParamOuter>(fieldType: ModelFieldType) {
  * If not specified on create operations, a ULID will be auto-generated service-side.
  * @returns ID field definition
  */
-export function id(): ModelField<Nullable<string>> {
-  return _field(ModelFieldType.Id);
+export function id(): ModelField<Nullable<string>, never, undefined, ModelFieldType.Id> {
+  return _field<Nullable<string>, ModelFieldType.Id>(ModelFieldType.Id);
 }
 
 /**
  * A string scalar type that is represented server-side as a UTF-8 character sequence.
  * @returns string field definition
  */
-export function string(): ModelField<Nullable<string>> {
-  return _field(ModelFieldType.String);
+export function string(): ModelField<Nullable<string>, never, undefined, ModelFieldType.String> {
+  return _field<Nullable<string>, ModelFieldType.String>(ModelFieldType.String);
 }
 
 /**
  * An integer scalar type with a supported value range between -(2^31) and 2^31-1.
  * @returns integer field definition
  */
-export function integer(): ModelField<Nullable<number>> {
-  return _field(ModelFieldType.Integer);
+export function integer(): ModelField<Nullable<number>, never, undefined, ModelFieldType.Integer> {
+  return _field<Nullable<number>, ModelFieldType.Integer>(ModelFieldType.Integer);
 }
 
 /**
  * A float scalar type following represented server-side as an IEEE 754 floating point value.
  * @returns float field definition
  */
-export function float(): ModelField<Nullable<number>> {
-  return _field(ModelFieldType.Float);
+export function float(): ModelField<Nullable<number>, never, undefined, ModelFieldType.Float> {
+  return _field<Nullable<number>, ModelFieldType.Float>(ModelFieldType.Float);
 }
 
 /**
  * A boolean scalar type that can be either true or false.
  * @returns boolean field definition
  */
-export function boolean(): ModelField<Nullable<boolean>> {
-  return _field(ModelFieldType.Boolean);
+export function boolean(): ModelField<Nullable<boolean>, never, undefined, ModelFieldType.Boolean> {
+  return _field<Nullable<boolean>, ModelFieldType.Boolean>(ModelFieldType.Boolean);
 }
 
 /**
  * A date scalar type that is represented server-side as an extended ISO 8601 date string in the format `YYYY-MM-DD`.
  * @returns date field definition
  */
-export function date(): ModelField<Nullable<string>> {
-  return _field(ModelFieldType.Date);
+export function date(): ModelField<Nullable<string>, never, undefined, ModelFieldType.Date> {
+  return _field<Nullable<string>, ModelFieldType.Date>(ModelFieldType.Date);
 }
 
 /**
  * A time scalar type that is represented server-side as an extended ISO 8601 time string in the format `hh:mm:ss.sss`.
  * @returns time field definition
  */
-export function time(): ModelField<Nullable<string>> {
-  return _field(ModelFieldType.Time);
+export function time(): ModelField<Nullable<string>, never, undefined, ModelFieldType.Time> {
+  return _field<Nullable<string>, ModelFieldType.Time>(ModelFieldType.Time);
 }
 
 /**
  * A date time scalar type that is represented server-side as an extended ISO 8601 date and time string in the format `YYYY-MM-DDThh:mm:ss.sssZ`.
  * @returns datetime field definition
  */
-export function datetime(): ModelField<Nullable<string>> {
-  return _field(ModelFieldType.DateTime);
+export function datetime(): ModelField<Nullable<string>, never, undefined, ModelFieldType.DateTime> {
+  return _field<Nullable<string>, ModelFieldType.DateTime>(ModelFieldType.DateTime);
 }
 
 /**
  * A timestamp scalar type that is represented by an integer value of the number of seconds before or after `1970-01-01-T00:00Z`.
  * @returns timestamp field definition
  */
-export function timestamp(): ModelField<Nullable<number>> {
-  return _field(ModelFieldType.Timestamp);
+export function timestamp(): ModelField<Nullable<number>, never, undefined, ModelFieldType.Timestamp> {
+  return _field<Nullable<number>, ModelFieldType.Timestamp>(ModelFieldType.Timestamp);
 }
 
 /**
  * An email scalar type that is represented server-side in the format `local-part@domain-part` as defined by RFC 822.
  * @returns email field definition
  */
-export function email(): ModelField<Nullable<string>> {
-  return _field(ModelFieldType.Email);
+export function email(): ModelField<Nullable<string>, never, undefined, ModelFieldType.Email> {
+  return _field<Nullable<string>, ModelFieldType.Email>(ModelFieldType.Email);
 }
 
 /**
@@ -307,8 +341,8 @@ export function email(): ModelField<Nullable<string>> {
  * rather than as the literal input strings.
  * @returns JSON field definition
  */
-export function json(): ModelField<Nullable<Json>> {
-  return _field(ModelFieldType.JSON);
+export function json(): ModelField<Nullable<Json>, never, undefined, ModelFieldType.JSON> {
+  return _field<Nullable<Json>, ModelFieldType.JSON>(ModelFieldType.JSON);
 }
 
 /**
@@ -317,8 +351,8 @@ export function json(): ModelField<Nullable<Json>> {
  * to the North American Numbering Plan.
  * @returns phone number field definition
  */
-export function phone(): ModelField<Nullable<string>> {
-  return _field(ModelFieldType.Phone);
+export function phone(): ModelField<Nullable<string>, never, undefined, ModelFieldType.Phone> {
+  return _field<Nullable<string>, ModelFieldType.Phone>(ModelFieldType.Phone);
 }
 
 /**
@@ -326,8 +360,8 @@ export function phone(): ModelField<Nullable<string>> {
  * URLs must contain a schema (http, mailto) and can't contain two forward slashes (//) in the path part.
  * @returns URL field definition
  */
-export function url(): ModelField<Nullable<string>> {
-  return _field(ModelFieldType.Url);
+export function url(): ModelField<Nullable<string>, never, undefined, ModelFieldType.Url> {
+  return _field<Nullable<string>, ModelFieldType.Url>(ModelFieldType.Url);
 }
 
 /**
@@ -336,6 +370,6 @@ export function url(): ModelField<Nullable<string>> {
  * to indicate subnet mask.
  * @returns IP address field definition
  */
-export function ipAddress(): ModelField<Nullable<string>> {
-  return _field(ModelFieldType.IPAddress);
+export function ipAddress(): ModelField<Nullable<string>, never, undefined, ModelFieldType.IPAddress> {
+  return _field<Nullable<string>, ModelFieldType.IPAddress>(ModelFieldType.IPAddress);
 }
