d5ae07f63f4550aa0d77a4a488b8d7f953e308de
Bobby Yu <bobbyu@amazon.com>
2025-03-12 19:24:29 -0700

    bump perf test benchmark

--- packages/benches/p50/operations/p50-prod-CRUDL.bench.ts
+++ packages/benches/p50/operations/p50-prod-CRUDL.bench.ts
@@ -629,4 +629,4 @@ bench('prod p50 CRUDL', async () => {
   });
 
   await client.models.Todo.list();
-}).types([684214, 'instantiations']);
+}).types([692058, 'instantiations']);
--- packages/benches/p50/operations/p50-prod-selection-set.bench.ts
+++ packages/benches/p50/operations/p50-prod-selection-set.bench.ts
@@ -632,4 +632,4 @@ bench('prod p50 CRUDL', async () => {
   });
 
   await client.models.Todo.list({ selectionSet });
-}).types([707395, 'instantiations']);
+}).types([715279, 'instantiations']);
