9fca3b164f5419781f59f954451c11a24e563fe7
James Jarvis <jjarvisp@amazon.com>
2025-04-14 11:35:13 -0700

    chore: revert run e2e test

--- scripts/stop_npm_proxy.ts
+++ scripts/stop_npm_proxy.ts
@@ -2,7 +2,6 @@ import { execa, execaCommand } from 'execa';
 
 const VERDACCIO_PORT = 4873;
 const NPM_REGISTRY = 'https://registry.npmjs.org/';
-const YARN_REGISTRY = 'https://registry.yarnpkg.com/';
 
 /**
  * Kills the process that is listening on VERDACCIO_PORT
@@ -14,7 +13,6 @@ const YARN_REGISTRY = 'https://registry.yarnpkg.com/';
  * https://github.com/sindresorhus/execa/blob/HEAD/docs/escaping.md
  */
 await execa('npm', ['config', 'set', 'registry', NPM_REGISTRY]);
-await execa('yarn', ['config', 'set', 'registry', YARN_REGISTRY]);
 
 // returns the process id of the process listening on the specified port
 let pid: number;
